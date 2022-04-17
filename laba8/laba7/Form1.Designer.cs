3f805f42215dce8c3c5f3eb55672c8026a683a28
DENISSP3KTR <54879762+DENISSP3KTR@users.noreply.github.com>
2022-04-17 18:13:49 +0900

    laba8

---
+++ laba8/laba7/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿
+namespace laba8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 95);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(742, 360);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(27, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(22, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Преподы";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(163, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Студенты";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(168, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Отобразить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(264, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(22, 472);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(266, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Идентификационный номер";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(22, 507);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Курс";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(22, 543);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(185, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Место проживания";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(22, 580);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(149, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Дата рождения";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(22, 67);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(160, 25);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Текущий список:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(179, 67);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 25);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Преподы";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(807, 37);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 49);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(921, 37);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(91, 49);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(807, 107);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(91, 49);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "3";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(921, 107);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(91, 49);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "4";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(867, 176);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(91, 49);
+            this.button7.TabIndex = 18;
+            this.button7.Text = "5";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(694, 461);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 19;
+            this.button8.Text = "Очистить";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1033, 634);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Университет";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        public System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ laba8/laba7/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using laba8;
+using Npgsql;
+using DataModels;
+namespace laba8
+{
+    public partial class Form1 : Form
+    {
+        DB db = new DB();
+        NpgsqlDataAdapter da = null;
+        DataTable table = null;
+        public Form1()
+        {
+            InitializeComponent();
+            db.opencon();
+            da = new NpgsqlDataAdapter("select distinct city, univ_id from lectures", db.GetConnection());
+            table = new DataTable();
+            da.Fill(table);
+            comboBox1.DataSource = table;
+            comboBox1.DisplayMember = "city";
+            comboBox1.ValueMember = "univ_id";
+            comboBox1.SelectedIndex = -1;
+            db.closecon();
+            toolTip1.SetToolTip(this.button3, "Вывести сведения о студентах, фамилии, либо имена которых начинаются на «И».");
+            toolTip1.SetToolTip(this.button4, "Вывести данные об оценках студентов второго курса, отсортированные по фамилии.");
+            toolTip1.SetToolTip(this.button5, "Вывести фамилии преподавателей и названия предметов которые они ведут.");
+            toolTip1.SetToolTip(this.button6, "Вывести фамилии и имена студентов, обучающихся на курсе не ниже 3 и получающих наибольшую стипендию.");
+            toolTip1.SetToolTip(this.button7, "Рассчитать среднюю оценку студентов, полученную на экзамене по информатике.");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label8.Text = "Преподы";
+            string whereStr = String.Format("where lectures.city='{0}' and university.univ_id=lectures.univ_id",this.comboBox1.Text);
+            string query = String.Format("select lectures.surname as Фамилия, lectures.name as Имя, lectures.city as Город, university.univ_name as Университет from lectures, university " + " {0}", whereStr);
+            db.opencon();
+            da = new NpgsqlDataAdapter(query, db.GetConnection());
+            table = new DataTable();
+            da.Fill(table);
+            dataGridView1.DataSource = table;
+            db.closecon();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label8.Text = "Студенты";
+            table = new DataTable();
+            table.Clear();
+            db.opencon();
+            da = new NpgsqlDataAdapter("select student.surname as Фамилия, student.name as Имя, university.univ_name as Университет from student, university where student.univ_id=university.univ_id;", db.GetConnection());
+            da.Fill(table);
+            dataGridView1.DataSource = table;
+            db.closecon();
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            label3.Text = "Идентификационный номер: ";
+            label4.Text = "Курс: ";
+            label5.Text = "Место проживания: ";
+            label6.Text = "Дата рождения: ";
+            if (label8.Text == "Студенты" && dataGridView1.CurrentCell.ColumnIndex == 0)
+            {
+                table = new DataTable();
+                table.Clear();
+                db.opencon();
+                string whereStr1 = String.Format("where surname='{0}' ", dataGridView1.CurrentCell.Value.ToString());
+                string whereStr2 = String.Format("and name='{0}';", dataGridView1.CurrentRow.Cells[1].Value.ToString());
+                string whereStr = whereStr1 + whereStr2;
+                string query = String.Format("select student_id, kurs, city, birthday from student" + " {0}", whereStr);
+                NpgsqlCommand comm = new NpgsqlCommand(query, db.GetConnection());
+                da = new NpgsqlDataAdapter(comm);
+                da.Fill(table);
+                NpgsqlDataReader dr = comm.ExecuteReader();
+                while (dr.Read())
+                {
+                    label3.Text += dr["student_id"].ToString();
+                    label4.Text += dr["kurs"].ToString();
+                    label5.Text += dr["city"].ToString();
+                    label6.Text += dr["birthday"].ToString();
+                }
+                db.closecon();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddStudent frm = new AddStudent();
+            frm.ShowDialog();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            using (var bd = new Baza1DB())
+            {
+                var q =
+                    (from c in bd.Students
+                    where c.Name.StartsWith("И") || c.Surname.StartsWith("И")
+                    select c).ToList();
+
+                dataGridView1.DataSource = q;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (var bd = new Baza1DB())
+            {
+                var q =
+                    (from c in bd.Students
+                     where c.Kurs == 2
+                     orderby c.Surname
+                     join p in bd.ExamMarks on c.StudentId equals p.StudentId
+                     select new { c.Surname, p.Mark }).ToList();
+
+                dataGridView1.DataSource = q;
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            using (var bd = new Baza1DB())
+            {
+                var q =
+                    (from c in bd.Lectures
+                     join p in bd.Subjects on c.UnivId equals p.SubjId
+                     select new { c.Surname, p.SubjName }).ToList();
+
+                dataGridView1.DataSource = q;
+            }
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            using (var bd = new Baza1DB())
+            {
+                var maxx = bd.Students.Select(c => c.Stipend).Max();
+                var q =
+                    (from c in bd.Students
+                     where c.Kurs>2 && c.Stipend == maxx
+                     select new {c.Name, c.Surname, c.Kurs, c.Stipend}).ToList();
+
+                dataGridView1.DataSource = q;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            using (var bd = new Baza1DB())
+            {
+                var q = bd.ExamMarks.Where(c => c.SubjId == 10).Select(c => c.Mark).Average();
+
+                MessageBox.Show(q.ToString());
+            }
+        }
+    }
+}
