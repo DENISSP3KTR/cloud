2324c6656cc0cd342f076d76c9343e7547297cef
DENISSP3KTR <54879762+DENISSP3KTR@users.noreply.github.com>
2022-04-25 02:16:54 +0900

    .net laba9

---
+++ laba9/laba9/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+namespace laba9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenFile();
+        }
+        public void OpenFile()
+        {
+            Excel excel = new Excel(@"D:\github\cloud\laba9\asd.xlsx",1);
+            MessageBox.Show(excel.ReadCell(0, 0));
+        }
+    }
+}
