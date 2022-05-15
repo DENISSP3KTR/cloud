f446a180d29ae4949180cc9d10d64787badf9f19
DENISSP3KTR <djspektr2001@mail.ru>
2022-05-16 02:37:54 +0900

    laba 10

---
+++ laba10/laba10/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace laba10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.новоеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.рисованиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.рандомныйКругToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.концентрическиеКругиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.кругиНаОтрезкеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.рисованиеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1289, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.новоеToolStripMenuItem,
+            this.сохранитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // новоеToolStripMenuItem
+            // 
+            this.новоеToolStripMenuItem.Name = "новоеToolStripMenuItem";
+            this.новоеToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.новоеToolStripMenuItem.Text = "Новое";
+            this.новоеToolStripMenuItem.Click += new System.EventHandler(this.новоеToolStripMenuItem_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // рисованиеToolStripMenuItem
+            // 
+            this.рисованиеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.рандомныйКругToolStripMenuItem,
+            this.концентрическиеКругиToolStripMenuItem,
+            this.кругиНаОтрезкеToolStripMenuItem});
+            this.рисованиеToolStripMenuItem.Name = "рисованиеToolStripMenuItem";
+            this.рисованиеToolStripMenuItem.Size = new System.Drawing.Size(78, 20);
+            this.рисованиеToolStripMenuItem.Text = "Рисование";
+            // 
+            // рандомныйКругToolStripMenuItem
+            // 
+            this.рандомныйКругToolStripMenuItem.Name = "рандомныйКругToolStripMenuItem";
+            this.рандомныйКругToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.рандомныйКругToolStripMenuItem.Text = "Рандомный круг";
+            this.рандомныйКругToolStripMenuItem.Click += new System.EventHandler(this.рандомныйКругToolStripMenuItem_Click);
+            // 
+            // концентрическиеКругиToolStripMenuItem
+            // 
+            this.концентрическиеКругиToolStripMenuItem.Name = "концентрическиеКругиToolStripMenuItem";
+            this.концентрическиеКругиToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.концентрическиеКругиToolStripMenuItem.Text = "Концентрические круги";
+            this.концентрическиеКругиToolStripMenuItem.Click += new System.EventHandler(this.концентрическиеКругиToolStripMenuItem_Click);
+            // 
+            // кругиНаОтрезкеToolStripMenuItem
+            // 
+            this.кругиНаОтрезкеToolStripMenuItem.Name = "кругиНаОтрезкеToolStripMenuItem";
+            this.кругиНаОтрезкеToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.кругиНаОтрезкеToolStripMenuItem.Text = "Круги на отрезке";
+            this.кругиНаОтрезкеToolStripMenuItem.Click += new System.EventHandler(this.кругиНаОтрезкеToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1230, 568);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1236, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1236, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1289, 592);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem новоеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem рисованиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem рандомныйКругToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem концентрическиеКругиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem кругиНаОтрезкеToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ laba10/laba10/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace laba10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap image;
+        Graphics g;
+        Pen pen;
+        Pen pen1;
+        SolidBrush b;
+        Point p1;
+        Point p2;
+        bool cl1 = true;
+        bool cl2 = false;
+        public void новоеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = image;
+            g = Graphics.FromImage(image);
+            g.Clear(Color.White);
+        }
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opd = new OpenFileDialog();
+            opd.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+            if (opd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    image = new Bitmap(opd.FileName);
+                    Bitmap image2 = new Bitmap(image, pictureBox1.Width, pictureBox1.Height);
+                    pictureBox1.Image = image2;
+                    pictureBox1.Invalidate();
+                    g = Graphics.FromImage(image2);
+                }
+                catch
+                {
+                    DialogResult rezult = MessageBox.Show("Невозможно открыть выбранный файл", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                SaveFileDialog svd = new SaveFileDialog();
+                svd.Title = "Сохранить картинку как...";
+                svd.OverwritePrompt = true;
+                svd.CheckPathExists = true;
+                svd.Filter = "Image Files(*.BMP)|*.BMP|Image Files(*.JPG)|*.JPG|Image Files(*.GIF)|*.GIF|Image Files(*.PNG)|*.PNG|All files (*.*)|*.*";
+                svd.ShowHelp = true;
+                if (svd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        pictureBox1.Image.Save(svd.FileName, ImageFormat.Jpeg);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Невозможно сохранить изображение", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (g != null)
+            {
+                if (b != null)
+                {
+                    Random r = new Random();
+                    int re = r.Next(100);
+                    b.Color = Randcolor();
+                    g.FillEllipse(b, e.X, e.Y, re, re);
+                }
+                else if (pen != null)
+                {
+                    Random r = new Random();
+                    int rad = r.Next(20, 150), x, y;
+                    for (int i=rad; i>=10; i -= 5)
+                    {
+                        x = e.X - i;
+                        y = e.Y - i;
+                        g.DrawEllipse(pen, x, y, i * 2, i * 2);
+                    }
+                }
+                else if (pen1 != null)
+                {
+                    if (cl1)
+                    {
+                        p1 = e.Location;
+                        cl1 = false;
+                    }
+                    if (cl2)
+                    {
+                        p2 = e.Location;
+                        cl1 = true;
+                        g.DrawLine(pen1, p1, p2);
+                        int rad = 100;
+                        Point pa = new Point();
+                        pa = minlen(p1, p2, rad);
+                        int a = rad / 2 - pa.Y;
+                        int b = rad / 2 - pa.X;
+                        g.DrawEllipse(pen1, p1.X - a, p1.Y - b, rad, rad);
+                    } 
+                }
+                else
+                {
+                    MessageBox.Show("Выберите что рисовать");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Создайте новый холст или откройте изображение");
+            }
+            pictureBox1.Invalidate();
+            label1.Text = p1.X.ToString() + " " + p1.Y.ToString();
+            label2.Text = p2.X.ToString() + " " + p2.Y.ToString();
+        }
+        public static double DistanceBetweenTwoPoints(Point point1, Point point2)
+        {
+            int dx = point1.X - point2.X;
+            int dy = point1.Y - point2.Y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+        public Point minlen(Point p1, Point p2, double rad)
+        {
+            double d = DistanceBetweenTwoPoints(p1, p2);
+            double a, b;
+            a = Math.Abs(p2.Y - p1.Y);
+            b = Math.Abs(p2.X - p1.X);
+            double sinalf, cosalf;
+            Point p = new Point();
+            sinalf = a / d;
+            cosalf = b / d;
+            a = rad / 2 * sinalf;
+            b = rad / 2 * cosalf;
+            p.X = (int)a;
+            p.Y = (int)b;
+            return p;
+        }
+        public void wPointdraw(Point p1, Point p2)
+        {
+            g.DrawLine(pen1, p1.X, p1.Y, p2.X, p2.Y);
+        }
+        private void рандомныйКругToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pen = null;
+            pen1 = null;
+            Color c = Randcolor();
+            b = new SolidBrush(c);
+        }
+        public Color Randcolor()
+        {
+            Color c = new Color();
+            Random r = new Random();
+            c = Color.FromArgb(r.Next(255), r.Next(255), r.Next(255));
+            return c;
+        }
+
+        private void концентрическиеКругиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            b = null;
+            pen1 = null;
+            pen = new Pen(Randcolor(), 3.0f);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void кругиНаОтрезкеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            b = null;
+            pen = null;
+            pen1 = new Pen(Randcolor(), 2.0f);
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (!cl1)
+            {
+                cl2 = true;
+            }
+            else
+            {
+                cl2 = false;
+            }
+        }
+    }
+}
