492208981f311a4e6b5eaa7a759ee8c6ed1214b3
Denis Osipov <54879762+DENISSP3KTR@users.noreply.github.com>
2023-09-18 11:20:30 +0900

    commit

---
+++ яп и сис програ/laba1/laba1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+void zad1()
+{
+    int s = 0;
+    for (int i = 0; i <= 1000; i++)
+    {
+        if (i % 7 == 0)
+        {
+            s += i;
+        }
+    }
+    Console.WriteLine(s);
+}
+zad1();
+
+void zad2()
+{
+
+}
